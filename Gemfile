fe57d0fbb41876f3b92db90920f1433bf6383f74
Apprentice <apprentice@dbc09.local>
2013-10-22 15:29:49 -0700

    add activity validation flash feedback

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rspec'
 gem 'capybara'
 
 gem 'sinatra'
+gem 'rack-flash3'
 gem 'activerecord'
 gem 'sinatra-activerecord'
 gem 'pg'
--- do_something_app.rb
+++ do_something_app.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'rack-flash'
 
 require_relative 'models/activity'
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || "postgres://localhost/do_something_dev")
 
 enable :sessions
+use Rack::Flash
 
 
 class User < ActiveRecord::Base
@@ -19,12 +21,20 @@ get '/' do
 end
 
 get '/create_activity' do
+  if flash[:notice]
+    @errors = flash[:notice]
+  end
  erb :create_activity
 end
 
 post '/create_activity' do
-  Activity.create(params)
-  redirect '/'
+  new_activity = Activity.new(params)
+  if new_activity.save
+    redirect('/')
+  else
+    flash[:notice] = new_activity.errors.messages
+    redirect('/create_activity')
+  end
 end
 
 post '/signup' do
