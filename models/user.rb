a8564797fbe23f1a89dd123e7e90c10e5c6953a9
Apprentice <apprentice@dbc09.local>
2013-10-22 18:23:09 -0700

    Add activerecord and HTML5 validations for user

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,12 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
+  validates :first_name, :email, :password_hash, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/,
+                              message: "must be a valid email" }
+
+
 
   include BCrypt
 
