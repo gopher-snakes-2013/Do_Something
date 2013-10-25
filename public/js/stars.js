4bf3734c05278bd79546df59e5ccaa7fc33d9879
Steven Nugent <stevennugentdev@gmail.com>
2013-10-24 17:05:22 -0700

    Use JS to convert ratings to stars

---
+++ public/js/stars.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	
+	function ratingToStars() {
+		var $scores = $('.rating_score')
+		for (var i=0;i<$scores.length;i++)
+		{ 	$score = $scores[i].innerText
+			$scoreInteger = parseInt($score)
+			if ($scoreInteger === 5){
+				$scores[i].innerText = "★★★★★"
+			} else if ($scoreInteger === 4){
+				$scores[i].innerText = "★★★★"				
+			} else if ($scoreInteger === 3){
+				$scores[i].innerText = "★★★"				
+			} else if ($scoreInteger === 2){
+				$scores[i].innerText = "★★"				
+			} else if ($scoreInteger === 1){
+				$scores[i].innerText = "★"				
+			}
+		}
+	}
+
+	ratingToStars();
+});
