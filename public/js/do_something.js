2cf3f5a977fa2c38c06bbcd657d8e0e6f5ba90a6
Apprentice <anniesing1014@gmail.com>
2013-10-25 15:05:00 -0700

    first release css

--- public/js/do_something.js
+++ public/js/do_something.js
@@ -4,10 +4,17 @@ $(document).ready(function(){
       url: "activities/select",
       type: "post", 
       success: function(activity_id){
-
-      $(".activity").css("background-color", "white");
-      $("#activity_"+activity_id).css("background-color", "lightblue");
-    }
+        $(".activity_pieces").css({
+                                    "background-color": "rgba(255,255,255,0.7)",
+                                    "color": "rgba(0, 0, 0, 0.6)",
+                                    "border": "none"
+                                    });
+        $("#activity_"+activity_id).css({
+                                          "background-color" : "lemonchiffon",
+                                          "border" : "4px solid rgba(50, 66, 98, 0.8)",
+                                          "color": "black"
+                                        });
+      }
     })
     .fail(function(){
       alert("You don't have any activities to choose from! Please add activities.");
